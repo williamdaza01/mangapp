2b08dd9e98219753195887781cea4c58c6be36be
unknown <juanjo.op1771@gmai.com>
2023-02-26 23:33:33 -0500

    Login y Register terminados

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from "react-dom";
+import React from "react";
 import "./login.css";
-import { Login } from "./login.jsx";
 
+import { Login } from "./login";
+import { Registro } from "./registro";
+
+//ReactDOM.render(<Login />, document.getElementById("root"));
 ReactDOM.render(<Login />, document.getElementById("root"));
